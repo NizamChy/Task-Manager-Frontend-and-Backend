a3c5797500a7be3dad04f26d497807f8681d7841
NizamChy <nizam.chy.cs@gmail.com>
2026-06-11 01:04:32 +0600

    updated frontend

--- frontend/Task-Manager/src/pages/Auth/SignUp.jsx
+++ frontend/Task-Manager/src/pages/Auth/SignUp.jsx
@@ -1,5 +1,58 @@
+import { useState } from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import { validateEmail } from "../../utils/helper";
+import ProfilePhotoSelector from "../../components/inputs/ProfilePhotoSelector";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [adminInviteToken, setAdminInviteToken] = useState("");
+
+  const [error, setError] = useState(null);
+
+  // Handle Signup Form Submit
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    if (!fullName) {
+      setError("Please enter full name.");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    setError("");
+
+    // Signup API Call
+  };
+
+  return (
+    <AuthLayout>
+      <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Create an account</h3>
+
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Join us today by entering your details below.
+        </p>
+
+        <form onSubmit={handleSignUp}>
+          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4"></div>
+        </form>
+      </div>
+    </AuthLayout>
+  );
 };
 
 export default SignUp;
